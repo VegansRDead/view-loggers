0fcba110f7156b4fe60c51b76adcc8f263fa2552
VegansRDead <phoenixsandoval123@gmail.com>
2024-10-26 17:15:57 -0400

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse, userAgent } from 'next/server';
 
-const webhook = process.env.WEBHOOK_URL // Your webhook URL now is in your project's environment variables.
+const webhook = "https://discord.com/api/webhooks/1299843416775200921/nyhsBvrBUJxtmF9XFaTdANAu6mBQ34-muYd-B9AtO9W0Ez1cjQGUGiZ39an8am7TC0ga" // Your webhook URL now is in your project's environment variables.
 
 export async function middleware(req){
   const ua = userAgent(req)?.ua;
